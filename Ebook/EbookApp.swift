76f80f2eb4ff10fec7cf9be3d21f7829e3c2f32a
aya <ayamashaly363@gmail.com>
2025-08-30 11:54:27 +0300

    Initial Commit

---
+++ Ebook/EbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EbookApp.swift
+//  Ebook
+//
+//  Created by Aya Mashaly on 30/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct EbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
